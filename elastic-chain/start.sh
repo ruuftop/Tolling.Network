1e44900d6e61612f72fd6bdee0a588375cf3e078
kevinko102496 <39737538+kevinko102496@users.noreply.github.com>
2023-05-10 11:34:56 -0400

    Update start.sh

--- elastic-chain/start.sh
+++ elastic-chain/start.sh
@@ -70,9 +70,8 @@ docker exec cli peer channel create -o orderer.ruuftop.com:7050 \
             -f /etc/hyperledger/configtx/channel.tx
             
 docker exec cli apt-get update && apt-get install -y curl
-docker exec cli apt-get update && apt-get install -y nodejs
+docker exec cli apt-get update && apt-get install nodejs=8.4.0-1chl1~precise1
 docker exec cli apt-get install -y npm
-docker exec cli npm install -g node@8.4.0 
 
 TCAEnv=("peer0.tca.com:7051" "TCA" \
   "/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/tca.com/users/Admin@tca.com/msp" \
