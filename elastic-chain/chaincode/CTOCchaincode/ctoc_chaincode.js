5c4bc42d7e13956229c0ad519cafc10f9f11ef72
kevinko102496 <39737538+kevinko102496@users.noreply.github.com>
2023-05-25 23:12:19 +0400

    Update ctoc_chaincode.js

--- elastic-chain/chaincode/CTOCchaincode/ctoc_chaincode.js
+++ elastic-chain/chaincode/CTOCchaincode/ctoc_chaincode.js
@@ -56,6 +56,7 @@ var Chaincode = class {
     let source = "TCA";
     let result = await stub.getPrivateData(source.toLowerCase() + "_toll_charges_source", tag_id);
     console.info(result);
+    console.info(result.toString('utf8'));
 
     return result;
   }
@@ -65,6 +66,7 @@ var Chaincode = class {
     let source = "BATA";
     let result = await stub.getPrivateData(source.toLowerCase() + "_toll_charges_destination", tag_id);
     console.info(result);
+    console.info(result.toString('utf8'));
 
     return result;
   }
